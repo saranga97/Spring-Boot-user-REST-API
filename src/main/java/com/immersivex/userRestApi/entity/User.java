aba5788b9c2764c320c6093f5fa54e0164f062e9
sarangan <sarangan@codegen.net>
2023-12-04 23:04:37 +0530

    Initial commit

---
+++ src/main/java/com/immersivex/userRestApi/entity/User.java
@@ -0,0 +1,13 @@
+package com.immersivex.userRestApi.entity;
+
+import jakarta.persistence.Entity;
+
+@Entity
+@
+
+public class User {
+
+    public int id;
+    public string name;
+    public string address;
+}
